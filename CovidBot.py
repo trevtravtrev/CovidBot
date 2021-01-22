1af2b543158f6fd32e0738ce22bde9e33a9315af
Trevor White <trevor.white@wayne.edu>
2021-01-21 20:53:05 -0500

    Added docstrings for readability

--- CovidBot.py
+++ CovidBot.py
@@ -5,11 +5,20 @@ from time import sleep
 
 
 def open_website():
+    """
+    Opens Henry Ford Hospital MyChart Scheduling link.
+    :return: n/a
+    """
     webbrowser.open('https://mychart.hfhs.org/MyChart/Scheduling')
     sleep(4)
 
 
 def click_button(button_image, delay=1):
+    """
+    :param button_image: image name string (must be stored in same directory as code)
+    :param delay: delay in seconds after button is clicked
+    :return: n/a
+    """
     location = pyautogui.locateCenterOnScreen(button_image, confidence=.5)
     pyautogui.moveTo(location)
     pyautogui.click()
@@ -17,10 +26,18 @@ def click_button(button_image, delay=1):
 
 
 def check_appointment():
+    """
+    Checks for "no available appointments" image on screen.
+    :return: returns coordinate location if "no available appointments" image is found, returns None if appointments found
+    """
     return pyautogui.locateOnScreen('no_appointment.png', confidence=.5)
 
 
 def main():
+    """
+    Main function that performs the correct sequence to auto-fill forms and check if there are available appointments.
+    :return: n/a
+    """
     try:
         open_website()
         while True:
@@ -52,7 +69,7 @@ def main():
                 click_button('start_over.png', delay=3)
                 continue
             else:
-                print("Appointments found!!!")
+                print("Appointments available!!!")
                 break
 
     except Exception as e:
